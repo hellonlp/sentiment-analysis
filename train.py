2aaa2929e82add4d97b19c966e5297d139e99799
Ming <319407@qq.com>
2020-07-24 16:44:06 +0800

    Update train.py

--- train.py
+++ train.py
@@ -20,25 +20,25 @@ MODEL = NetworkAlbert(is_training=True )
 
 
 
-## 训练集
+# 训练集
 input_ids,input_masks,segment_ids,label_ids = get_features()
 input_ids_test,input_masks_test,segment_ids_test,label_ids_test = get_features_test()
 print('1'*10)
-## 训练数据和测试数据
+# 训练数据和测试数据
 N = len(input_ids)
 N_train = N
 ids_train = np.arange(N_train)               
 num_batches = int((N_train - 1) /hp.batch_size) + 1
 print('number of batch:',num_batches)
-## 测试集
+# 测试集
 ids_test = np.arange(len(input_ids_test)) 
-## 启动图和训练
+# 启动图和训练
 saver = tf.train.Saver(max_to_keep=100)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 
-## 恢复模型参数
+# 恢复模型参数
 MODEL_SAVE_PATH = os.path.join(pwd,'model')
 ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH)
 if ckpt and ckpt.model_checkpoint_path:
@@ -47,7 +47,7 @@ if ckpt and ckpt.model_checkpoint_path:
 
 
 with sess.as_default():
-    for i in range(hp.n_epoch):
+    for i in range(hp.num_train_epochs):
         indexs = shuffle_one(ids_train)       
         for batch_num in range(num_batches-1):
             i1 = indexs[batch_num * hp.batch_size:min((batch_num + 1) * hp.batch_size, N_train)]
@@ -69,14 +69,14 @@ with sess.as_default():
                 saver.save(sess, os.path.join(pwd, 'model', 'model_%s_%s.ckpt'%(str(i),str(batch_num))))
             # Print
             if batch_num % hp.print_step == 0:
-                ## Loss of Train data 
+                # Loss of Train data 
                 fd = {MODEL.input_ids: input_id_,
                       MODEL.input_masks: input_mask_ ,
                       MODEL.segment_ids:segment_id_,
                       MODEL.label_ids:label_id_}
                 loss = sess.run(MODEL.loss, feed_dict = fd)
                 print('Time:',time_now_string(),'Epoch:',i,'Batch number:',batch_num,'Loss(train):',loss)
-                ##  Loss of Test data
+                # Loss of Test data
                 indexs_test = shuffle_one(ids_test)[:128]
                 input_id_test = select(input_ids_test,indexs_test)
                 input_mask_test = select(input_masks_test,indexs_test)
